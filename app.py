3df05c88ee49a14d1a4a05587179d0e355b43cac
Drishty Gupta <24f3005089@ds.study.iitm.ac.in>
2026-05-19 06:50:51 +0000

    fix csv issue

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import csv
+import os
 
 app = FastAPI()
 
@@ -8,29 +9,33 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Load CSV data
 students_data = []
 
-with open("students.csv", newline="", encoding="utf-8") as csvfile:
-    reader = csv.DictReader(csvfile)
+csv_path = "students.csv"
 
-    for row in reader:
-        students_data.append({
-            "studentId": int(row["studentId"]),
-            "class": row["class"]
-        })
+if os.path.exists(csv_path):
+    with open(csv_path, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            students_data.append({
+                "studentId": int(row["studentId"]),
+                "class": row["class"]
+            })
 
 @app.get("/")
 async def home():
     return {"message": "Student API running"}
 
 @app.get("/api")
-async def get_students(class_: list[str] | None = Query(default=None, alias="class")):
-    
+async def get_students(
+    class_: list[str] | None = Query(default=None, alias="class")
+):
+
     if class_:
         filtered = [
             student for student in students_data
